6ca7c22fb94d0729f6a3803c078b76190b19b66e
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2022-10-22 23:39:14 +0100

    add eureka dependency for registring the gateway itself

---
+++ gateway/src/main/java/me/zakaria/gateway/GatewayConfig.java
@@ -0,0 +1,15 @@
+package me.zakaria.gateway;
+
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(ReactiveDiscoveryClient reactiveDiscoveryClient, DiscoveryLocatorProperties discoveryLocatorProperties) {
+        return new DiscoveryClientRouteDefinitionLocator(reactiveDiscoveryClient,discoveryLocatorProperties);
+    }
+}
